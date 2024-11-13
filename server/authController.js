cdf7b14a597a8a0f6fef39507d9446a20e210bd9
stweeeezy1 <stenlohvart402@gmail.com>
2024-11-13 21:06:28 +0200

    some progress

--- server/authController.js
+++ server/authController.js
@@ -1,25 +1,52 @@
-const User = require('./models/User');
-const Role = require('./models/Role');
+const User = require("./models/User");
+const Role = require("./models/Role");
+const bcrypt = require("bcryptjs");
 
 class authController {
   async registration(req, res) {
     try {
-    } catch (e) {}
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "That username already exists" });
+      }
+
+      const hashPassword = bcrypt.hashSync(password, 7);
+      const userRole = await Role.findOne({ value: "USER" });
+
+      if (!userRole) {
+        return res.status(404).json({ message: "Role not found" });
+      }
+
+      const user = new User({
+        username,
+        password: hashPassword,
+        roles: [userRole._id],
+      });
+
+      await user.save();
+      return res.json({ message: "User was successfully signed up" });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: "Registration error" });
+    }
   }
 
   async login(req, res) {
     try {
-    } catch (e) {}
+    } catch (e) {
+      res.status(400).json({ message: "Login error" });
+    }
   }
 
   async getUsers(req, res) {
     try {
-      const userRole = new Role()
-      const adminRole = new Role({value: "ADMIN"})
-      await userRole.save()
-      await adminRole.save()
-      res.json("server work");
-    } catch (e) {}
+      res.json("Server works");
+    } catch (e) {
+      res.status(400).json({ message: "Error fetching users" });
+    }
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./authRouter");
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5004;
 
 const app = express();
 
