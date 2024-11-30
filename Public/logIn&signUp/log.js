bfa7098c5121c4c4b6c04d55a945d7322f2f7a76
stweeeezy1 <stenlohvart402@gmail.com>
2024-11-30 16:52:13 +0200

    fix

--- Public/logIn&signUp/log.js
+++ Public/logIn&signUp/log.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", async (event) => {
 
   try {
     const response = await fetch("https://todo-list-12.onrender.com", {
-      method: "POST",
+      method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
